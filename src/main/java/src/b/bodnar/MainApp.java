86cf31c2ac2d366628a0a49ab65495809c892f72
Bohdan Bodnar <iloveyoulviv@gmail.com>
2019-12-26 10:21:39 +0200

    Add class Executor

--- src/main/java/src/b/bodnar/MainApp.java
+++ src/main/java/src/b/bodnar/MainApp.java
@@ -1,21 +1,11 @@
 package src.b.bodnar;
 
-import src.b.bodnar.model.Person;
-import src.b.bodnar.service.ExcelReader;
-import src.b.bodnar.service.WordWriter;
-
-import java.io.File;
-import java.util.List;
+import src.b.bodnar.execute.Executor;
 
 public class MainApp {
 
     public static void main(String[] args) {
 
-        File src = new File("C:/Users/b.bodnar/Desktop/Acts.xlsx");
-        File template = new File("C:/Users/b.bodnar/Desktop/Acts.xlsx");
-
-        List<Person> people =  ExcelReader.excelReader(src);
-        WordWriter wordWriter = new WordWriter();
-        wordWriter.createDoc("C:/Users/b.bodnar/Desktop/template.docx", people);
+        Executor.execute("zamarstynivska.xlsx");
     }
 }
